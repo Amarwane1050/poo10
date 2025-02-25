68db026078638e061a6a5dfc894e447e4bf8415a
marwane1050 <mabdelkader@student.efp.be>
2025-02-25 21:06:44 +0100

    creations des routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\gamecontroller;
+
+// Route pour afficher tous les jeux
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
+
+// Route pour afficher le formulaire de création d'un jeu
+Route::get('/games/create', [GameController::class, 'create'])->name('games.create');
+
+// Route pour enregistrer un nouveau jeu
+Route::post('/games', [GameController::class, 'store'])->name('games.store');
+
+// Route pour afficher un jeu spécifique
+Route::get('/games/{games}', [GameController::class, 'show'])->name('games.show');
+
+// Route pour afficher le formulaire d'édition d'un jeu
+Route::get('/games/{games}/edit', [GameController::class, 'edit'])->name('games.edit');
+
+// Route pour mettre à jour un jeu existant
+Route::put('/games/{games}', [GameController::class, 'update'])->name('games.update');
+
+// Route pour supprimer un jeu
+Route::delete('/games/{games}', [GameController::class, 'destroy'])->name('games.destroy');
+
